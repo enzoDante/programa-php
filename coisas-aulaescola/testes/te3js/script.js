837f510a1bd682c5c5c9fbed22d8c80f591be0a0
Enzo Dante <85023505+enzoDante@users.noreply.github.com>
2022-09-26 17:00:31 -0300

    novo

---
+++ coisas-aulaescola/testes/te3js/script.js
@@ -0,0 +1,7 @@
+function func(){
+    let combo = document.getElementById("combo")
+    let opcao = document.createElement("option")
+    opcao.innerHTML = "texto"
+    opcao.value = 2
+    combo.appendChild(opcao)
+}
